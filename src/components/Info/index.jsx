50c46927a265029e30353a19f9343b26b36787de
Marian Fedkovych <m.fed@intobi.com>
2024-04-04 08:35:42 +0300

    <a>

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -1,14 +1,14 @@
 const Info = () => {
   return (
     <div className="w-full flex items-start justify-between">
-      <p>(001)</p>
-      <div className="text-left">
+      <p><a target="_blank" href="https://www.surreal4you.com/">(002)</a></p>
+      <div className="text-left md:block hidden">
         <p>Unique & Convenient</p>
         <p>Design Solutions</p>
       </div>
-      <div className="text-left">
+      <div className="text-center md:text-left">
         <p>THIS_PAGE_IS_%</p>
-        <p>UNDER_DEVELOPMENT</p>
+        <p>UNDER_CONSTRUCTION</p>
       </div>
       <p>.com</p>
     </div>
