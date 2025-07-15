cc1e0aef5c44c4571135db0122b2bb261817832e
[dyad] <marianfedkovich@gmail.com>
2025-07-15 10:21:09 +0300

    [dyad] wrote 4 file(s)

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import ContactUsPage from "./pages/ContactUsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import { BackToTopButton } from "./components"; // Import BackToTopButton
+import { BackToTopButton, CustomCursor } from "./components"; // Import BackToTopButton and CustomCursor
 
 const App = () => {
   return (
@@ -17,6 +17,7 @@ const App = () => {
       <Analytics />
       <SpeedInsights />
       <BackToTopButton /> {/* Render BackToTopButton here globally */}
+      <CustomCursor /> {/* Render CustomCursor here globally */}
     </Router>
   );
 };
