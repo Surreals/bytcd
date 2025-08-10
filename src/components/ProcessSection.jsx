294cc3b6906e28a71caac99ee3eb2a31b21d199c
Marian Fedkovich <marianfedkovich@gmail.com>
2025-08-10 03:29:45 +0300

    [dyad] Adjusted the scroll speed of the timeline line in the ProcessSection - wrote 1 file(s)

--- src/components/ProcessSection.jsx
+++ src/components/ProcessSection.jsx
@@ -34,7 +34,7 @@ const ProcessSection = ({ id }) => {
             trigger: timelineRef.current,
             start: "top 75%",
             end: "bottom 25%",
-            scrub: 0.5,
+            scrub: true, // Changed from 0.5 to true for direct scroll syncing
           },
         }
       );
