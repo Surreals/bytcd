b7be055112c9b7ea1bd20e1e54a9a93414d59025
Marian Fedkovich <marianfedkovich@gmail.com>
2025-08-10 03:35:41 +0300

    [dyad] Slowed down the background line animation in the ProcessSection - wrote 1 file(s)

--- src/components/ProcessSection.jsx
+++ src/components/ProcessSection.jsx
@@ -32,9 +32,9 @@ const ProcessSection = ({ id }) => {
           transformOrigin: "top",
           scrollTrigger: {
             trigger: timelineRef.current,
-            start: "top 60%", // Adjusted to start later
+            start: "top 60%",
             end: "bottom 25%",
-            scrub: 0.5,
+            scrub: 1.5, // Increased from 0.5 to 1.5 to slow down the animation
           },
         }
       );
