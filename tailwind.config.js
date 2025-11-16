3f1c2b29cefe81785afeb9aec5ca0363e0fc4ca0
Marian Fedkovich <marianfedkovich@gmail.com>
2025-11-16 13:01:50 +0200

    [dyad] wrote 3 file(s)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'archivo-black': ['Archivo Black', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
